dac8b6b8504675b2130d29fc038255b9b59752f1
Edward Elliott <edelliott@nc.rr.com>
2015-07-14 10:28:31 -0400

    Got Salesforce.com connected.

--- GeoLocationUpdater/Program.cs
+++ GeoLocationUpdater/Program.cs
@@ -13,10 +13,21 @@ namespace GeoLocationUpdater
 
 		static void Main(string[] args)
 		{
+			// Clear the console
+			Console.Clear();
+
 			// Login to Salesforce.com
 			loginToSF("eelliott@lenovo.com.FULL", "1W2kGuru*0XTfsYiBtLvCFzHCq77cfCGfL");
 
 			// Get list of SMSC BP - North America accounts
+			getSMSC_BPAccounts(10);
+
+			// Wait for a key press to exit.
+			Console.WriteLine();
+			Console.WriteLine();
+			Console.WriteLine("Please press a key to exit....");
+
+			Console.ReadKey();
 		}
 
 		private static void loginToSF(string userName, string password)
@@ -38,7 +49,7 @@ namespace GeoLocationUpdater
 			}
         }
 
-		private static List<Account> getSMSC_BPAccounts(int numberOfAccount = 100)
+		private static List<Account> getSMSC_BPAccounts(int numberOfAccounts = 100)
 		{
 			List<Account> accountList = null;
 
@@ -48,6 +59,19 @@ namespace GeoLocationUpdater
 				{
 					accountList = new List<Account>();
 	            }
+
+				var soqlQuery = string.Format(	"SELECT Id, Name, ShippingStreet, ShippingCity, ShippingState, " + Environment.NewLine +
+												"       ShippingCountry, ShippingPostalCode, " + Environment.NewLine +
+												"       BillingStreet, BillingCity, BillingState, " + Environment.NewLine +
+												"       BillingCountry, BillingPostalCode, " + Environment.NewLine +
+												"       AccountLocation__Latitude__s, AccountLocation__Longitude__s " + Environment.NewLine +
+												"FROM Account " + Environment.NewLine +
+												"ORDER BY Name " + Environment.NewLine +
+												"LIMIT {0}", numberOfAccounts);
+
+				var results = sforceSvc.query(soqlQuery);
+
+				Console.WriteLine(results.ToString());
 			}
 			catch(Exception ex)
 			{
