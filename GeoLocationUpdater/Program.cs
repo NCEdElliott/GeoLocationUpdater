16d9a399ccb2094e7cd0ed4967621a2415ddb518
Edward Elliott <edelliott@nc.rr.com>
2015-07-14 09:57:34 -0400

    Latest progress on GeoLocationUpdater utility.

---
+++ GeoLocationUpdater/Program.cs
@@ -0,0 +1,64 @@
+﻿using SForce;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoLocationUpdater
+{
+	class Program
+	{
+		private static SforceService sforceSvc { get; set; }
+
+		static void Main(string[] args)
+		{
+			// Login to Salesforce.com
+			loginToSF("eelliott@lenovo.com.FULL", "1W2kGuru*0XTfsYiBtLvCFzHCq77cfCGfL");
+
+			// Get list of SMSC BP - North America accounts
+		}
+
+		private static void loginToSF(string userName, string password)
+		{ 
+			try
+			{
+				if (sforceSvc == null)
+				{
+					sforceSvc = new SforceService();
+				}
+
+				sforceSvc.login(userName, password);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Error logging in to Salesforce.com   Error: {0}\nInnerException: {1}", 
+					ex.Message,
+					ex.InnerException != null ? ex.InnerException.Message : string.Empty);
+			}
+        }
+
+		private static List<Account> getSMSC_BPAccounts(int numberOfAccount = 100)
+		{
+			List<Account> accountList = null;
+
+			try
+			{
+				if (accountList == null)
+				{
+					accountList = new List<Account>();
+	            }
+			}
+			catch(Exception ex)
+			{
+				Console.WriteLine("Error in getSMSC_BPAccounts().  Error: {0}\nInnerException: {1}", 
+					ex.Message,
+					ex.InnerException != null ? ex.InnerException.Message : string.Empty);
+
+				return null;
+			}
+
+			return accountList;
+		}
+	}
+}
