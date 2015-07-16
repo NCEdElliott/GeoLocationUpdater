04ceda9f35f43a71cb321f6b93870ff54376649a
Edward Elliott <edelliott@nc.rr.com>
2015-07-15 23:06:39 -0400

    Latest

--- GeoLocationUpdater/Program.cs
+++ GeoLocationUpdater/Program.cs
@@ -17,11 +17,28 @@ namespace GeoLocationUpdater
 			// Clear the console
 			Console.Clear();
 
+			List<Account> accounts = null;
+
 			// Login to Salesforce.com
 			loginToSF("eelliott@lenovo.com.FULL", "1W2kGuru*0XTfsYiBtLvCFzHCq77cfCGfL");
 
 			// Get list of SMSC BP - North America accounts
-			getSMSC_BPAccounts(10);
+			accounts = getSMSC_BPAccounts(10);
+
+			Console.WriteLine("Number of Accounts: {0}", accounts.Count);
+
+			geoLocateAccounts(accounts);
+
+			while (accounts != null &&
+					accounts.Count > 0)
+			{
+				// Get list of SMSC BP - North America accounts
+				accounts = getSMSC_BPAccounts(10);
+
+				Console.WriteLine("Number of Accounts: {0}", accounts.Count);
+
+				geoLocateAccounts(accounts);
+			}
 
 			// Wait for a key press to exit.
 			Console.WriteLine();
@@ -70,14 +87,18 @@ namespace GeoLocationUpdater
 				if (accountList == null)
 				{
 					accountList = new List<Account>();
-	            }
+				}
 
-				var soqlQuery = string.Format(	"SELECT Id, Name, ShippingStreet, ShippingCity, ShippingState, " + Environment.NewLine +
+				var soqlQuery = string.Format("SELECT Id, Name, RecordType.Name, " + Environment.NewLine +
+												"       ShippingStreet, ShippingCity, ShippingState, " + Environment.NewLine +
 												"       ShippingCountry, ShippingPostalCode, " + Environment.NewLine +
 												"       BillingStreet, BillingCity, BillingState, " + Environment.NewLine +
 												"       BillingCountry, BillingPostalCode, " + Environment.NewLine +
 												"       AccountLocation__Latitude__s, AccountLocation__Longitude__s " + Environment.NewLine +
 												"FROM Account " + Environment.NewLine +
+												"WHERE (AccountLocation__Latitude__s = NULL OR AccountLocation__Latitude__s = 0) AND " + Environment.NewLine +
+												"      (AccountLocation__Longitude__s = NULL OR AccountLocation__Longitude__s = 0) AND " + Environment.NewLine +
+												"       RecordType.Name = 'SMSC BP - North America' " +
 												"ORDER BY Name " + Environment.NewLine +
 												"LIMIT {0}", numberOfAccounts);
 
@@ -85,15 +106,18 @@ namespace GeoLocationUpdater
 
 				if (results != null)
 				{
+
 					foreach (Account acct in results.records)
 					{
 						Console.WriteLine(acct.Name);
+
+						accountList.Add(acct);
 					}
 				}
 			}
-			catch(Exception ex)
+			catch (Exception ex)
 			{
-				Console.WriteLine("Error in getSMSC_BPAccounts().  Error: {0}\nInnerException: {1}", 
+				Console.WriteLine("Error in getSMSC_BPAccounts().  Error: {0}\nInnerException: {1}",
 					ex.Message,
 					ex.InnerException != null ? ex.InnerException.Message : string.Empty);
 
@@ -101,6 +125,11 @@ namespace GeoLocationUpdater
 			}
 
 			return accountList;
+		}
+		
+		private static void geoLocateAccounts(List<Account> accounts)
+		{
+
 		}
 	}
 }
